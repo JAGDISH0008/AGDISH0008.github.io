02c866899349b297329c62c51604dd8959389c9b
JAGDISH0008 <jagadeesh26597@gmail.com>
2020-07-19 09:20:57 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HeroComponent } from './hero/hero.component';
+import { NavService } from './service/nav.service';
+import { AboutComponent } from './about/about.component';
+import { FactsComponent } from './facts/facts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProjectsComponent,
+    HeroComponent,
+    AboutComponent,
+    FactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'project',component: ProjectsComponent},
+    ])
   ],
-  providers: [],
+  providers: [NavService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavService } from '../service/nav.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  activeEl = 'intro'
+  constructor(private navService: NavService, private router: Router) {
+    const currentPath = window.location.pathname.split('/')[1]
+    if (currentPath !== '') {
+      this.activeEl = currentPath
+    }
   }
-
+  scroll(id: string) {
+    // TODO: figured out how to navigate on mobile
+    this.activeEl = id
+    if (this.router.url !== '/') {
+      this.router.navigate([''])
+      setTimeout(() => {
+        this.navService.scroll(id)
+        this.navService.target.next(null)
+      }, 1000)
+    } else {
+      this.navService.scroll(id)
+      this.navService.target.next(null)
+    }
+  }
 }
