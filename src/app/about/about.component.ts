7bef9610728875629f373b602d0fb30387ca2696
JAGDISH0008 <jagadeesh26597@gmail.com>
2021-07-03 16:51:17 +0530

    age update.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -13,12 +13,10 @@ export class AboutComponent implements OnInit {
 
   constructor() {
     this.calAge();
-    console.log(this.age);
-
   }
   calAge() {
-    this.age = (this.year - 1997) +
-      ((this.month - 5) > 0 ? 1 : 0)
+    var timeDiff = Math.abs(Date.now() - Date.UTC(97, 5, 26));
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
   }
 
   ngOnInit(): void {
